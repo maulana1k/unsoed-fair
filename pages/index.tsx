d2fb9f6c3fb101450250ad289e710a88edf5eb20
Tiannns <89785305+Tiannns@users.noreply.github.com>
2023-06-09 07:54:42 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,7 +37,7 @@ function Jobs() {
     return Router.push('/login')
   }
   return (
-    <div className="container h-full bg-violet-700">
+    <div className=" h-full bg-violet-700">
       <nav
         className={`${
           scrolled ? 'bg-white shadow-md' : 'bg-violet-700'
@@ -188,104 +188,91 @@ function Jobs() {
 }
 export default function Welcome() {
   return (
-    <main className={`flex min-h-screen flex-col items-center justify-between p-24`}>
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By <Image src="/vercel.svg" alt="Vercel Logo" className="dark:invert" width={100} height={24} priority />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div id="__next" className='h-full bg-violet-700'>
+      <main>
+        <nav id="navbar-main" className='fixed top-0 z-[100] flex h-[76px] w-full bg-transparent px-4 transition-all duration-300 lg:px-8 bg-violet-700'>
+          <div className='mx-auto flex w-full max-w-screen-laptop-lg items-center justify-between'>
+            <div className='flex'>
+              <a href="/">
+                <img src="#" alt="" />
+                <img src="#" alt="" />
+              </a>
+              <ul className='hidden items-center justify-center lg:ml-[27px] lg:flex'>
+                <li className='mb-3 items-center lg:mr-8 lg:mb-0'>
+                  <a href="#" className='mb-3 items-center justify-center text-sm font-semibold leading-none text-white'>Jobs</a>
+                </li>
+                <li className='mb-3 items-center lg:mr-8 lg:mb-0'>
+                  <a href="#" className='mb-3 items-center justify-center text-sm font-semibold leading-none text-white'>Companies</a>
+                </li>
+                <li className='mb-3 items-center lg:mr-8 lg:mb-0'>
+                  <a href="#" className='mb-3 items-center justify-center text-sm font-semibold leading-none text-white'>Career Fair</a>
+                </li>
+                <li className='mb-3 items-center lg:mr-8 lg:mb-0'>
+                  <a href="#" className='mb-3 items-center justify-center text-sm font-semibold leading-none text-white'>International Japan Trip</a>
+                </li>
+              </ul>
+            </div>
+            <ul className='navbar-menu-mobile flext items-center gap-1.5 kg:gap-0'>
+              <div className='header-menu-mobile mb-1 flex w-full items-center justify-between lg:mb-0'>
+                <a href="#" className='h-max'></a>
+                <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" className="iconify cursor-pointer text-xl text-[#BABABA] lg:hidden iconify iconify--ci" width="1em"  height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
+                  <path fill="currentColor" d="M17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41L17.59 5Z"></path>
+                </svg>
+              </div>
+              <li className='mb-0 w-full items-center text-center lg:mr-8 lg:hidden'>
+                <a href="#" className="flex w-full items-center justify-center py-[10px] text-sm font-medium leading-[18px] text-neutral-80 lg:p-0">Jobs</a>
+              </li>
+              <li className='mb-0 w-full items-center text-center lg:mr-8 lg:hidden'>
+                <a href="#" className="flex w-full items-center justify-center py-[10px] text-sm font-medium leading-[18px] text-neutral-80 lg:p-0">Companies</a>
+              </li>
+              <li className='mb-0 w-full items-center text-center lg:mr-8 lg:hidden'>
+                <a href="#" className="flex w-full items-center justify-center py-[10px] text-sm font-medium leading-[18px] text-neutral-80 lg:p-0">Career Fair</a>
+              </li>
+              <li className='mb-0 w-full items-center text-center lg:mr-8 lg:hidden'>
+                <a href="#" className="flex w-full items-center justify-center py-[10px] text-sm font-medium leading-[18px] text-neutral-80 lg:p-0">International Japan Trip</a>
+              </li>
+              <li className='mb-0 w-full items-center text-center lg:mr-8 lg:hidden'>
+                <a href="#" className="flex w-full items-center justify-center py-[10px] text-sm font-medium leading-[18px] text-neutral-80 lg:p-0">For Employers</a>
+              </li>
+              <li className='mb-0 w-full items-center text-center lg:mr-8 lg:hidden'>
+                <a href="#" className="flex w-full items-center justify-center py-[10px] text-sm font-medium leading-[18px] text-neutral-80 lg:p-0">Login</a>
+              </li>
+              <li className='hidden lg:list-item'>
+                <a href="/register" className=" flex h-[38px] w-max items-center justify-center rounded-[84px] border border-solid border-tertiary-violet-60 bg-white px-[26px] text-xs font-bold leading-none text-[#6913D8] lg:border-white ">Sign Up</a>
+              </li>
+              <hr className="mt-2 mb-[18px] w-full border-t border-solid border-t-[#D0D1D3] lg:hidden"/>
+              <div className='mb-1 flex w-full justify-center gap-4 lg:hidden'>
+                <a href="#" className='flex h-[36px] w-full max-w-[250px] items-center justify-center rounded-[84px] border border-solid border-[#6913D8] bg-white text-xs font-bold leading-none text-[#6913D8]'>Login</a>
+                <a href="#" className=" flex h-[36px] w-full max-w-[250px] items-center justify-center rounded-[84px] bg-[#6913D8] text-xs font-bold leading-none text-white ">Sign up</a>
+              </div>
+            </ul>
+            <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" className="iconify cursor-pointer text-2xl text-white lg:hidden iconify--eva custom-cursor-on-hover" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
+              <g id="iconifyreact396">
+                <g id="iconifyreact397">
+                  <g id="iconifyreact398" fill="currentColor">
+                    <rect width="18" height="2" x="3" y="11" rx=".95" ry=".95"></rect>
+                    <rect width="18" height="2" x="3" y="16" rx=".95" ry=".95"></rect>
+                    <rect width="18" height="2" x="3" y="6" rx=".95" ry=".95"></rect>
+                  </g>
+                </g>
+              </g>
+            </svg>
+          </div>
+        </nav>
+        <header className="section relative z-[5] flex w-full items-center overflow-hidden overflow-x-hidden bg-header bg-cover md:bg-none md:bg-auto  Header_header__Zqwz_ h-[505px] sm:h-[470px] lg:h-[766px] bg-violet-700">
+          <div className="container px-4 lg:px-[60px] 2xl:px-[30px]">
+            <div className='z-10'>
+              <div className="flex flex-col items-center gap-[60px] lg:flex-row">
+                <div className='order-2 lg:order-1 lg:w-3/5'>
+                  <h1 className="z-10 w-full text-[28px] font-bold leading-[1.3] text-white lg:text-[50px] lg:leading-[1.35]" >
+                  Let 450+ Top Indonesian Companies Approach You
+                  </h1>
+                </div>
+              </div>
+            </div>
+          </div>
+        </header>
+      </main>
+    </div>
   )
 }
