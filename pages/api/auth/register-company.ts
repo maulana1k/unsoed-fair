133ab916a09ef7c7644da57be73c60083c20b86a
Maulana Imamul Khaq <mm930199@gmail.com>
2023-06-20 17:30:05 +0700

    update register page

--- pages/api/auth/register-company.ts
+++ pages/api/auth/register-company.ts
@@ -4,7 +4,7 @@ import bcrypt from 'bcryptjs'
 import prisma from '@/lib/prisma'
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') {
-    return res.status(405).json({ message: 'Method not allowed' })
+    return res.status(405).send('Method not allowed coba')
   }
 
   const { fullname, email, password } = req.body
@@ -13,6 +13,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     // Check if the user already exists in the database
     const existingUser = await prisma.user.findUnique({ where: { email } })
 
+    console.log(existingUser)
     if (existingUser) {
       return res.status(400).send('User already exists')
     }
