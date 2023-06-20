278d30df7693197da6172ed161c8388d4747df21
Maulana Imamul Khaq <mm930199@gmail.com>
2023-06-20 17:30:20 +0700

    job post and get api

--- pages/api/jobs.ts
+++ pages/api/jobs.ts
@@ -3,7 +3,36 @@ import bcrypt from 'bcryptjs'
 import prisma from '@/lib/prisma'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ message: 'Method not allowed' })
+  if (req.method === 'GET') {
+    try {
+      // Add your logic to fetch the data from the database or any other source
+      const jobs = await prisma.job.findMany()
+
+      return res.status(200).json(jobs)
+    } catch (error) {
+      console.error('Error fetching jobs', error)
+      return res.status(500).json({ error: 'Error fetching jobs' })
+    }
+  }
+  if (req.method === 'POST') {
+    try {
+      const { title, location, experience, type, userId, detail } = req.body
+
+      const job = await prisma.job.create({
+        data: {
+          title,
+          location,
+          experience,
+          type,
+          detail,
+          userId,
+        },
+      })
+
+      return res.status(200).json(job)
+    } catch (error) {
+      console.error('Error creating job', error)
+      return res.status(500).json({ error: 'Error creating job' })
+    }
   }
 }
